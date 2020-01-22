6a54a492544abb5b0f19c0cb05f1b8c623025234
cookjo <jordan.cook@pioneer.com>
2020-01-21 20:15:14 -0600

    Use depth-first search instead of blind iteration; ignore all non-cellular organisms/sequences

--- ncbi_export.py
+++ ncbi_export.py
@@ -5,6 +5,7 @@ import json
 from progress.bar import ChargingBar as Bar
 # from progress.bar import IncrementalBar as Bar
 
+BAR_SUFFIX = '[%(index)d / %(max)d] [%(elapsed_td)s / %(eta_td)s]'
 
 # Download dump files from: ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip
 NAMES_DUMP = 'NCBI Taxonomy/names.dmp'
@@ -41,9 +42,14 @@ SORTED_COLS = [
 ]
 
 ROOT_TAX_ID = 1
+CELLULAR_ORGANISMS_TAX_ID = 131567
 BACTERIA_TAX_ID = 2
 ARCHAEA_TAX_ID = 2157
-CELLULAR_ORGANISMS_TAX_ID = 131567
+EUKARYOTA_TAX_ID = 2759
+
+# Estimate based on NCBI taxonomy browser, used for progress bar
+# Can't get an exact estimate ddirectly from dataframe without traversing the whole tree
+EUKARYOTE_TAXA_ESTIMATE = 1500000
 
 
 def flatten_ncbi_dumps():
@@ -86,50 +92,81 @@ def load_ncbi_dump(file_path, col_names, **kwargs):
     return df
 
 
-# TODO: This is inefficient, a parallelized DFS would be better, but too lazy for that right now
-def nodes_to_dict(df):
+def generate_tree(df):
     """
     Convert NCBI taxonomy node structure into a tree
+    First attempt: blind iteration over dataframe
     """
     print('Converting rows into a tree')
     tree = {}
-    bar = Bar('Processing', max=len(df.index), suffix='[%(index)d / %(max)d] [%(elapsed_td)s / %(eta_td)s]')
+    bar = Bar('Processing', max=len(df.index), suffix=BAR_SUFFIX)
 
-    def find_parents(row, lst):
+    def find_parents(taxon, lst):
         """
-        Get this row and all its parents as a list, highest ranks first
+        Get this taxon and all its parents as a list, highest ranks first
         """
         # Base case: (super)kingdom of eukaryotic cellular organisms: return it with its descendants
-        if row['tax_id'] == CELLULAR_ORGANISMS_TAX_ID:
+        if taxon['tax_id'] == CELLULAR_ORGANISMS_TAX_ID:
             return lst
         # Base case: viruses, sequences, and prokaryotes: discard it and all its descendants
-        elif row['tax_id'] in [ROOT_TAX_ID, BACTERIA_TAX_ID, ARCHAEA_TAX_ID]:
+        elif taxon['tax_id'] in [ROOT_TAX_ID, BACTERIA_TAX_ID, ARCHAEA_TAX_ID]:
             return []
 
         # Recursive case: find parent node and append current node
-        parent_row = df[df['tax_id'] == row['parent_tax_id']].iloc[0]
+        parent = df[df['tax_id'] == taxon['parent_tax_id']].iloc[0]
 
         # Skip past 'no rank' groups
-        if row['rank'] != 'no rank':
-            lst.insert(0, f"taxonomy:{row['rank']}={row['name']}")
-        return find_parents(parent_row, lst)
+        if taxon['rank'] != 'no rank':
+            lst.insert(0, f"taxonomy:{taxon['rank']}={taxon['name']}")
+        return find_parents(parent, lst)
 
-    for _, row in df.iterrows():
+    for _, taxon in df.iterrows():
         # Start over at root
         tree_node = tree
         # Populate tree with this clade and all its parents, starting with highest ranks first
-        for level in find_parents(row, []):
+        for level in find_parents(taxon, []):
             tree_node = tree_node.setdefault(level, {})
         bar.next()
     bar.finish()
 
     with open(TREE_FILE, 'w') as f:
         json.dump(tree, f, indent=2)
-    print(f'Hierarchical data written to {TREE_FILE}')
+    print(f'Data written to {TREE_FILE}')
+    return tree
+
+
+def generate_tree_dfs(df):
+    """
+    Convert NCBI taxonomy node structure into a tree using a depth-first search
+    """
+    bar = Bar('Processing', max=EUKARYOTE_TAXA_ESTIMATE, suffix=BAR_SUFFIX)
+
+    def find_children(taxon, tree_node):
+        """
+        Get a row and all its children as a dict
+        """
+        tax_label = f"taxonomy:{taxon['rank']}={taxon['name']}"
+        tree_node = {tax_label: {}}
+        children = df[df['parent_tax_id'] == taxon['tax_id']]
+        bar.next()
+
+        # Base case: no children; Recursive case: update tree with all children
+        for _, child_node in children.iterrows():
+            tree_node[tax_label].update(find_children(child_node, tree_node))
+        return tree_node
+
+    eukaryote_node = df[df['tax_id'] == EUKARYOTA_TAX_ID].iloc[0]
+    tree = find_children(eukaryote_node, {})
+    bar.finish()
+
+    with open(TREE_FILE, 'w') as f:
+        json.dump(tree, f, indent=2)
+    print(f'Data written to {TREE_FILE}')
     return tree
 
 
 if __name__ == '__main__':
     # df = flatten_ncbi_dumps()
     df = pd.read_csv(FLAT_FILE)
-    nodes_to_dict(df)
+    # generate_tree(df)
+    generate_tree_dfs(df)
