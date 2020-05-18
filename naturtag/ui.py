22ca522ae394af4085145e14cf8abfabe9de1460
Jordan Cook <jordan.cook@pioneer.com>
2020-05-18 17:51:08 -0500

    Move all screen and navigation-managing logic to App class; reuse a single toolbar and update during screen transitions

--- naturtag/ui.py
+++ naturtag/ui.py
@@ -14,8 +14,10 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.properties import ListProperty, StringProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.metrics import dp
 
-from kivymd.app import MDApp as App
+from kivymd.app import MDApp
+from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.imagelist import SmartTileWithLabel
 from kivymd.uix.snackbar import Snackbar
 
@@ -32,6 +34,7 @@ logger.addHandler(out_hdlr)
 
 ASSETS_DIR = join(dirname(dirname(__file__)), 'assets', '')
 KV_SRC_DIR = join(dirname(dirname(__file__)), 'kv')
+HOME_SCREEN = 'image_selector'
 IMAGE_FILETYPES = ['*.jpg', '*.jpeg', '*.png', '*.gif']
 INIT_WINDOW_SIZE = (1250, 800)
 MD_PRIMARY_PALETTE = 'Teal'
@@ -42,33 +45,36 @@ class ImageMetaTile(SmartTileWithLabel):
     metadata = ObjectProperty()
 
 
+def alert(text, **kwargs):
+    Snackbar(text=text, **kwargs).show()
+
+# TODO: Split this up into multiple classes
 class Controller(BoxLayout):
     """
-    Top-level UI element that controls most app behavior, except for window/theme behavior,
-    which is controlled by ImageTaggerApp
+    Top-level UI element that controls application state and logic,
+    excluding screens & navigation, which is managed by ImageTaggerApp
     """
     file_list = ListProperty([])
     file_list_text = StringProperty()
+    selected_image = ObjectProperty()
     selected_image_table = ObjectProperty()
 
-    def __init__(self, **kwargs):
+    def __init__(self, settings, inputs, image_previews, file_chooser, **kwargs):
         super().__init__(**kwargs)
-        # Initialize screens and store references to them
-        for name, cls in SCREENS.items():
-            Builder.load_file(join(KV_SRC_DIR, f'{name}.kv'))
-            new_screen = cls()
-            new_screen.controller = self
-            self.ids.screen_manager.add_widget(new_screen)
-            self.__setattr__(f'{name}_screen', new_screen)
+        self.settings = settings
+        self.inputs = inputs
+        self.image_previews = image_previews
+        self.file_chooser = file_chooser
 
-        # Add additional references to some nested objects for easier access
-        self.settings = self.settings_screen.ids
-        self.inputs = self.image_selector_screen.ids
-        self.image_previews = self.image_selector_screen.ids.image_previews
-        self.file_chooser = self.image_selector_screen.ids.file_chooser
-
-        # Automatically adjust image preview layout
-        self.image_previews.bind(minimum_height=self.image_previews.setter('height'))
+    # TODO: for testing only
+    def open_table(self):
+        MDDataTable(
+            column_data=[
+                ("No.", dp(30)),  ("Column 1", dp(30)), ("Column 2", dp(30)),
+                ("Column 3", dp(30)), ("Column 4", dp(30)), ("Column 5", dp(30)),
+            ],
+            row_data=[ (f"{i + 1}", "2.23", "3.65", "44.1", "0.45", "62.5") for i in range(50)],
+        ).open()
 
     def add_image(self, window=None, path=None):
         """ Add an image to the current selection, with deduplication """
@@ -85,7 +91,7 @@ class Controller(BoxLayout):
         # Update image previews
 
         metadata = MetaMetadata(path)
-        img = ImageMetaTile(source=path, metadata=metadata,  text=metadata.summary)
+        img = ImageMetaTile(source=path, metadata=metadata, text=metadata.summary)
         img.bind(on_touch_down=self.handle_image_click)
         self.image_previews.add_widget(img)
 
@@ -98,6 +104,7 @@ class Controller(BoxLayout):
         """ Remove an image from file list and image previews """
         self.file_list.remove(image.source)
         self.inputs.file_list_text_box.text = '\n'.join(self.file_list)
+        self.selected_image = None
         image.parent.remove_widget(image)
 
     # TODO: Apply image file glob patterns to dir
@@ -128,9 +135,8 @@ class Controller(BoxLayout):
             return
         elif touch.button == 'right':
             self.remove_image(instance)
-        # TODO: Implement metadata view
         else:
-            print('left mouse clicked')
+            self.selected_image = instance
 
     def reset(self):
         """ Clear all image selections """
@@ -143,8 +149,11 @@ class Controller(BoxLayout):
     def run(self):
         """ Run image tagging for selected images and input """
         settings = self.get_settings_dict()
+        if not self.file_list:
+            alert(f'Select images to tag')
+            return
         if not settings['observation_id'] and not settings['taxon_id']:
-            Snackbar(text=f'First select either an observation or an organism').show()
+            alert(f'Select either an observation or an organism to tag images with')
             return
         tag_images(
             settings['observation_id'],
@@ -160,34 +169,86 @@ class Controller(BoxLayout):
             f'Taxon ID: {settings["observation_id"]}' if settings['observation_id']
             else f'Observation ID: {settings["observation_id"]}'
         )
-        Snackbar(text=f'{len(self.file_list)} images tagged with metadata for {selected_id}').show()
+        alert(f'{len(self.file_list)} images tagged with metadata for {selected_id}')
 
 
-class ImageTaggerApp(App):
+class ImageTaggerApp(MDApp):
+    """
+    Manages window, theme, screen and navigation state; other application logic is
+    handled by Controller
+    """
+    nav_drawer = ObjectProperty()
+    screen_manager = ObjectProperty()
     toolbar = ObjectProperty()
 
     def build(self):
+        # Init screens and store references to them
+        screens = {}
         Builder.load_file(join(KV_SRC_DIR, 'main.kv'))
+        for screen_name, screen_cls in SCREENS.items():
+            Builder.load_file(join(KV_SRC_DIR, f'{screen_name}.kv'))
+            screens[screen_name] = screen_cls()
 
-        # Window and theme settings
-        controller = Controller()
+        # Init controller with references to some nested screen objects for easier access
+        controller = Controller(
+            settings=screens['settings'].ids,
+            inputs=screens[HOME_SCREEN].ids,
+            image_previews=screens[HOME_SCREEN].ids.image_previews,
+            file_chooser=screens[HOME_SCREEN].ids.file_chooser,
+        )
+
+        # Init screen manager and nav elements
+        self.nav_drawer = controller.ids.nav_drawer
+        self.screen_manager = controller.ids.screen_manager
+        self.toolbar = controller.ids.toolbar
+        for screen in screens.values():
+            self.screen_manager.add_widget(screen)
+            screen.controller = controller
+        self.home()
+
+        # Set some event bindings that can't (easily) by done in kvlang
+        controller.settings.dark_mode_chk.bind(active=self.toggle_dark_mode)
+        controller.image_previews.bind(minimum_height=controller.image_previews.setter('height'))
+
+        # Set Window and theme settings
         Window.bind(on_dropfile=controller.add_image)
         Window.size = INIT_WINDOW_SIZE
         self.theme_cls.primary_palette = MD_PRIMARY_PALETTE
         self.theme_cls.accent_palette = MD_ACCENT_PALETTE
-        controller.settings.dark_mode_chk.bind(active=self.toggle_dark_mode)
 
-        controller.ids.screen_manager.current = 'image_selector'
-        self.toolbar = controller.ids.toolbar
-        # controller.ids.screen_manager.current = 'settings'
-        # TODO: Better help text that disappears as soon as an image or another screen is selected
-        Snackbar(
-            text=f'.{" " * 14}Drag and drop images or select them from the file chooser',
-            duration=10
-        ).show()
+        alert(  # TODO: make this disappear as soon as an image or another screen is selected
+            f'.{" " * 14}Drag and drop images or select them from the file chooser', duration=10)
         return controller
 
+    def home(self, *args):
+        self.switch_screen(HOME_SCREEN)
+
+    def open_nav(self, *args):
+        self.nav_drawer.set_state('open')
+
+    def close_nav(self, *args):
+        self.nav_drawer.set_state('close')
+
+    def switch_screen(self, screen_name):
+        self.screen_manager.current = screen_name
+        self.update_toolbar(screen_name)
+        self.close_nav()
+
+    def update_toolbar(self, screen_name):
+        """ Modify toolbar in-place so it can be shared by all screens """
+        self.toolbar.title = screen_name.title()
+        if screen_name == HOME_SCREEN:
+            self.toolbar.left_action_items = [['menu', self.open_nav]]
+            self.toolbar.right_action_items = [
+                ['fullscreen', self.toggle_fullscreen],
+                ['dots-vertical', self.open_settings],
+            ]
+        else:
+            self.toolbar.left_action_items = [["arrow-left", self.home]]
+            self.toolbar.right_action_items = []
+
     def toggle_dark_mode(self, switch=None, is_active=False):
+        """ Toggle between light and dark themes """
         self.theme_cls.theme_style = 'Dark' if is_active else 'Light'
 
     def toggle_fullscreen(self, *args):
