053836c310f4a3827e997b9d12155450015330bd
Jordan Cook <jordan.cook@pioneer.com>
2020-11-29 14:34:44 -0600

    Make isort skip model init file, since imports must be in order of model dependencies

--- naturtag/models/__init__.py
+++ naturtag/models/__init__.py
@@ -4,11 +4,11 @@
 
 # Imported in order of dependencies
 from naturtag.models.base import BaseModel, aliased_kwarg, kwarg, timestamp
-from naturtag.models.identification import Identification
-from naturtag.models.image_metadata import ImageMetadata
-from naturtag.models.keyword_metadata import HIER_KEYWORD_TAGS, KEYWORD_TAGS, KeywordMetadata
-from naturtag.models.meta_metadata import MetaMetadata
-from naturtag.models.observation import Observation
 from naturtag.models.photo import Photo
 from naturtag.models.taxon import Taxon, get_icon_path
 from naturtag.models.user import User
+from naturtag.models.identification import Identification
+from naturtag.models.observation import Observation
+from naturtag.models.image_metadata import ImageMetadata
+from naturtag.models.keyword_metadata import KeywordMetadata, KEYWORD_TAGS, HIER_KEYWORD_TAGS
+from naturtag.models.meta_metadata import MetaMetadata
