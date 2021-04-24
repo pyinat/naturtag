203a4191b3814665503034bd45521f42afe297eb
Jordan Cook <jordan.cook@pioneer.com>
2021-04-24 17:14:21 -0500

    Make a separate 'pre-release' branch instead of building a pre-release version for every push to dev

--- naturtag/__init__.py
+++ naturtag/__init__.py
@@ -1,19 +1,5 @@
-from logging import getLogger
 from os import getenv
 
+# Set the pre-release version number, if set by CI job
 __version__ = '0.6.1'
-
-
-def get_prerelease_version(version: str) -> str:
-    """If we're running in a GitHub Action job on the dev branch, get a prerelease version using the
-    current build number. For example: ``1.0.0 -> 1.0.0-dev.123``
-    """
-    if getenv('GITHUB_REF') == 'refs/heads/dev':
-        build_number = getenv('GITHUB_RUN_NUMBER', '0')
-        version = f'{version}.dev{build_number}'
-        getLogger(__name__).info(f'Using pre-release version: {version}')
-    return version
-
-
-# This won't modify the version outside of a GitHub Action job
-__version__ = get_prerelease_version(__version__)
+__version__ += getenv('PRE_RELEASE_SUFFIX', '')
