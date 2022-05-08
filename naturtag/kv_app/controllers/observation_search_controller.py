a9b54b048933e72765ab44c5cbe85946a0e09723
Jordan Cook <jordan.cook@pioneer.com>
2022-05-08 16:54:06 -0500

    Remove previous GUI attempt (saved in 'kivy' branch)

--- naturtag/kv_app/controllers/observation_search_controller.py
+++
@@ -1,6 +0,0 @@
-class ObservationSearchController:
-    """Controller class to manage observation search and display"""
-
-    def __init__(self, screen):
-        self.screen = screen
-        self.selected_observation = None
--- naturtag/models/__init__.py
+++ naturtag/models/__init__.py
@@ -3,7 +3,6 @@
 
 from pyinaturalist.models import Observation
 
-from naturtag.models.taxon import Taxon, get_icon_path
 from naturtag.models.image_metadata import ImageMetadata
-from naturtag.models.keyword_metadata import KeywordMetadata, KEYWORD_TAGS, HIER_KEYWORD_TAGS
+from naturtag.models.keyword_metadata import HIER_KEYWORD_TAGS, KEYWORD_TAGS, KeywordMetadata
 from naturtag.models.meta_metadata import MetaMetadata
