f18b66f835adc403c4ab1aa50989e599a24bf2fd
Jordan Cook <jordan.cook@pioneer.com>
2022-05-14 21:07:47 -0500

    Add rank filters

--- naturtag/controllers/__init__.py
+++ naturtag/controllers/__init__.py
@@ -1,2 +1,3 @@
+# flake8: noqa: F401
 from naturtag.controllers.image_controller import ImageController
 from naturtag.controllers.taxon_controller import TaxonController
--- naturtag/controllers/taxon_search.py
+++ naturtag/controllers/taxon_search.py
@@ -1,10 +1,11 @@
 """Components for searching for taxa"""
 from logging import getLogger
+from typing import Optional
 
-from pyinaturalist import IconPhoto
+from pyinaturalist import RANKS, IconPhoto
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QGroupBox, QPushButton, QSizePolicy
+from PySide6.QtWidgets import QComboBox, QGroupBox, QLabel, QPushButton, QSizePolicy
 
 from naturtag.app.style import fa_icon
 from naturtag.constants import SELECTABLE_ICONIC_TAXA
@@ -13,6 +14,10 @@ from naturtag.settings import Settings
 from naturtag.widgets import GridLayout, HorizontalLayout, PixmapLabel, TaxonAutocomplete, VerticalLayout
 
 logger = getLogger(__name__)
+
+# TODO: Option to show all ranks
+ignore_terms = ['sub', 'super', 'infra', 'epi', 'hybrid']
+COMMON_RANKS = [r for r in RANKS if not any([k in r for k in ignore_terms])][::-1]
 
 
 class TaxonSearch(VerticalLayout):
@@ -47,6 +52,18 @@ class TaxonSearch(VerticalLayout):
         group_box.setFixedWidth(400)
         group_box.setLayout(rank_layout)
         self.addWidget(group_box)
+        self.exact_rank = RankList('Exact')
+        self.min_rank = RankList('Minimum')
+        self.max_rank = RankList('Maximum')
+        rank_layout.addLayout(self.exact_rank)
+        rank_layout.addLayout(self.min_rank)
+        rank_layout.addLayout(self.max_rank)
+
+        # Clear exact rank after selecting min or max, and vice versa
+        self.min_rank.dropdown.activated.connect(self.exact_rank.reset)
+        self.max_rank.dropdown.activated.connect(self.exact_rank.reset)
+        self.exact_rank.dropdown.activated.connect(self.min_rank.reset)
+        self.exact_rank.dropdown.activated.connect(self.max_rank.reset)
 
         # Search/reset buttons
         button_layout = HorizontalLayout()
@@ -54,22 +71,27 @@ class TaxonSearch(VerticalLayout):
         search_button.setIcon(fa_icon('fa.search'))
         search_button.clicked.connect(self.search)
         button_layout.addWidget(search_button)
+
         reset_button = QPushButton('Reset')
         reset_button.setIcon(fa_icon('mdi.backspace'))
         reset_button.clicked.connect(self.reset)
         button_layout.addWidget(reset_button)
         self.addLayout(button_layout)
+
         self.addStretch()
 
     def search(self):
         """Search for taxa with the currently selected filters"""
         taxa = INAT_CLIENT.taxa.search(
             q=self.autocomplete.search_input.text(),
-            iconic_taxa=self.category_filters.selected_iconic_taxa,
-            locale=self.settings.locale,
+            taxon_id=self.category_filters.selected_iconic_taxa,
+            rank=self.exact_rank.text,
+            min_rank=self.min_rank.text,
+            max_rank=self.max_rank.text,
             preferred_place_id=self.settings.preferred_place_id,
+            locale=self.settings.locale,
         ).limit(10)
-        logger.info(taxa)
+        logger.info('\n'.join([str(t) for t in taxa]))
 
     def reset(self):
         """Reset all search filters"""
@@ -87,8 +109,8 @@ class IconicTaxonFilters(GridLayout):
             self.add_widget(button)
 
     @property
-    def selected_iconic_taxa(self):
-        return [t.name for t in self.widgets if t.isChecked()]
+    def selected_iconic_taxa(self) -> list[int]:
+        return [t.taxon_id for t in self.widgets if t.isChecked()]
 
     def reset(self):
         for widget in self.widgets:
@@ -112,3 +134,21 @@ class IconicTaxonButton(QPushButton):
         self.setFixedSize(50, 50)
         self.setContentsMargins(0, 0, 0, 0)
         self.setToolTip(name)
+
+
+class RankList(HorizontalLayout):
+    """Taxonomic rank dropdown"""
+
+    def __init__(self, label: str):
+        super().__init__()
+        self.addWidget(QLabel(label))
+        self.dropdown = QComboBox()
+        self.dropdown.addItems([''] + COMMON_RANKS[::-1])
+        self.addWidget(self.dropdown)
+
+    def reset(self):
+        self.dropdown.setCurrentIndex(0)
+
+    @property
+    def text(self) -> Optional[str]:
+        return self.dropdown.currentText() or None
