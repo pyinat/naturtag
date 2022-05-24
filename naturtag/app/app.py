237acc28bfc532c4d5e5237a08b3c232441035a5
Jordan Cook <jordan.cook@pioneer.com>
2022-05-24 13:36:45 -0500

    Fix taskbar icon and image loading from sys.argv

--- naturtag/app/app.py
+++ naturtag/app/app.py
@@ -109,9 +109,7 @@ class MainWindow(QMainWindow):
         shortcut.activated.connect(self.reload_qss)
 
         # Load any valid image paths provided on command line (or from drag & drop)
-        if __file__ in sys.argv:
-            sys.argv.remove(__file__)
-        self.image_controller.gallery.load_images(sys.argv)
+        self.image_controller.gallery.load_images([a for a in sys.argv if a != __file__])
 
         # Load demo images
         demo_images = list((ASSETS_DIR / 'demo_images').glob('*.jpg'))
