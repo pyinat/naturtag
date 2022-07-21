eda6a02ba080e1cc539b3f0b94a977a48009edb6
Jordan Cook <jordan.cook.git@proton.me>
2022-07-21 16:39:09 -0500

    Update windows build script

--- packaging/build_win.ps1
+++ packaging/build_win.ps1
@@ -1,13 +1,17 @@
 # Build pyinstaller package prior to creating Windows installer
 
-# Setup virtualenv
+# Create virtualenv
 # python -m venv  $env:USERPROFILE\.virtualenvs\naturtag
 
+# Install poetry
+# Invoke-WebRequest -Uri https://install.python-poetry.org -OutFile install-poetry.py
+# python install-poetry.py --preview
+# ~\AppData\Roaming\Python\Scripts\poetry.exe config virtualenvs.create false
+
+# Install dependencies
 . ~\.virtualenvs\naturtag\Scripts\activate.ps1
-pip install -U poetry
 poetry install -v --no-dev
-pip install -U setuptools
-pip install -U pyinstaller
+pip install -U setuptools pyinstaller
 pyinstaller -y packaging\naturtag.spec
 
 # Launch Actual Installer, then:
