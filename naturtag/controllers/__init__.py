c1bc467923a70b707e5dd903afac0c7298dbf698
Jordan Cook <jordan.cook.git@proton.me>
2022-08-26 21:40:14 -0500

    Observation search placeholder

--- naturtag/controllers/__init__.py
+++ naturtag/controllers/__init__.py
@@ -4,6 +4,7 @@
 from naturtag.controllers.base_controller import BaseController
 from naturtag.controllers.image_gallery import ImageGallery
 from naturtag.controllers.image_controller import ImageController
+from naturtag.controllers.observation_search import ObservationSearch
 from naturtag.controllers.observation_view import ObservationInfoSection
 from naturtag.controllers.observation_controller import ObservationController
 from naturtag.controllers.taxon_search import TaxonSearch
