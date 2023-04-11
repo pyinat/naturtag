858875158c4eb248ca13e889384fdf76bdb52bac
Jordan Cook <jordan.cook.git@proton.me>
2023-04-10 21:52:53 -0500

    Use bash for Windows build; manually add ruby gems to PATH for FPM

--- packaging/build_fpm.sh
+++ packaging/build_fpm.sh
@@ -14,7 +14,7 @@ mkdir -p $PKG_DIR/usr/share/applications
 mkdir -p $PKG_DIR/usr/share/icons/hicolor/scalable/apps
 cp -r $SRC_DIR $PKG_DIR/opt/
 cp $ASSETS_DIR/naturtag.desktop $PKG_DIR/usr/share/applications/
-cp $ASSETS_DIR/logo.svg $PKG_DIR/usr/share/icons/hicolor/scalable/apps/naturtag.svg
+cp $ASSETS_DIR/icons/logo.svg $PKG_DIR/usr/share/icons/hicolor/scalable/apps/naturtag.svg
 
 # Set file permissions
 find $PKG_DIR/opt/naturtag/ -type f -exec chmod 644 -- {} +
