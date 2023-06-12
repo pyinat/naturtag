5944410faaced03f83c8d84aa0dc7768b92489a6
Jordan Cook <jordan.cook.git@proton.me>
2023-06-12 15:12:33 -0500

    Add sprites packaged in a SQLite db

--- naturtag/utils/__init__.py
+++ naturtag/utils/__init__.py
@@ -2,4 +2,5 @@
 from naturtag.utils.i18n import read_locales
 from naturtag.utils.image_glob import get_valid_image_paths
 from naturtag.utils.parsing import get_ids_from_url, quote, strip_url
+from naturtag.utils.sprites import TAXON_SPRITES, get_sprite
 from naturtag.utils.thumbnails import generate_thumbnail
