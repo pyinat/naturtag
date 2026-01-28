34e8ae3d5b19653de1ad47d663fdb51848b26421
Jordan Cook <jordan.cook.git@proton.me>
2026-01-27 23:42:12 -0600

    Update packaged taxonomy db

--- packaging/export_taxa.sh
+++ packaging/export_taxa.sh
@@ -6,11 +6,12 @@ SRC_DB=$DATA_DIR/observations.db
 TAXON_CSV=taxon.csv
 TAXON_FTS_CSV=taxon_fts.csv
 ARCHIVE=assets/data/taxonomy.tar.gz
+MIN_OBSERVATIONS=20  # Only export taxa with at least this many RG observations
 
 echo 'Exporting Taxon table...'
 sqlite3 -header -csv $SRC_DB \
     "SELECT id, ancestor_ids, child_ids, iconic_taxon_id, leaf_taxa_count, observations_count_rg, name, parent_id, preferred_common_name, rank FROM taxon \
-     WHERE observations_count_rg >= 10 \
+     WHERE observations_count_rg >= $MIN_OBSERVATIONS \
      ;" \
     > $TAXON_CSV
 
