6e419460daa15199081c1fc3bf43e898ea41a24c
Jordan Cook <jordan.cook.git@proton.me>
2026-02-11 18:22:02 -0600

    Include raw file formats in image searches

--- naturtag/utils/__init__.py
+++ naturtag/utils/__init__.py
@@ -1,7 +1,7 @@
 # ruff: noqa: F401
 from naturtag.utils.click_help_colors import HelpColorsCommand, HelpColorsGroup
 from naturtag.utils.i18n import read_display_locales, read_locales
-from naturtag.utils.image_glob import get_valid_image_paths
+from naturtag.utils.image_glob import get_valid_image_paths, is_raw_path
 from naturtag.utils.parsing import get_ids_from_url, quote, strip_url
 from naturtag.utils.thumbnails import generate_thumbnail
 from naturtag.utils.updates import check_for_update, get_version
